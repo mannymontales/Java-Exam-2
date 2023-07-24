997ba788aaf43a789ddd97bda7a67cde98f50226
Manny <manny@Zipcoders-MacBook-Pro-58.local>
2023-07-24 10:39:57 -0400

    finished String ArrayUtilities

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 28/11/2018.
  */
@@ -10,8 +12,26 @@ public class StringArrayUtils {
      * @param endingIndex - ending index of array
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
-    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
-        return null;
+    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) throws IllegalArgumentException {
+        ArrayList<String> subArray = new ArrayList<>();
+
+        try {
+            for (int i = startingIndex; i < endingIndex; i++){
+                subArray.add(arrayToBeSpliced[i]);
+            }
+
+            //need to review try catch and exeptions
+            if (startingIndex < 0) throw new IllegalArgumentException();
+
+        } catch(IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException();
+        } catch(Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] newArr = new  String[subArray.size()];
+        newArr = subArray.toArray(newArr);
+        return newArr;
     }
 
 
@@ -21,6 +41,23 @@ public class StringArrayUtils {
      * @return an array all elements between after `startingIndex`
      */
     public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+        ArrayList<String> endingArray = new ArrayList<>();
+
+        try {
+            for (int i = startingIndex; i < arrayToBeSpliced.length; i++){
+                endingArray.add(arrayToBeSpliced[i]);
+            }
+            //review try catch and exceptions
+            //not sure why the last catch statement is not working need to review
+            if (startingIndex > arrayToBeSpliced.length) throw new IllegalArgumentException();
+        } catch(IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException();
+        } catch(IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] newArr = new  String[endingArray.size()];
+        newArr = endingArray.toArray(newArr);
+        return newArr;
     }
 }
